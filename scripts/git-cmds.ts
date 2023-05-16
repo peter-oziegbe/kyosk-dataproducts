b7e5dd907dbcfaed5af4e469e51760d8dd48754b
Nimrod Maina <nimrod.maina@kyosk.app>
2023-05-16 13:32:45 +0300

    - Fix dir bug

--- scripts/git-cmds.ts
+++ scripts/git-cmds.ts
@@ -8,6 +8,6 @@ export async function getCurrentBranch():Promise<string>{
 
 
 export async function getChangedRootPaths():Promise<string[]>{
-    let res = (await executeCmd('git diff HEAD HEAD^ --name-only | cut -d "/" -f2 | sort -u')).stdout
+    let res = (await executeCmd('git diff HEAD HEAD^ --name-only | cut -d "/" -f1 | sort -u')).stdout
     return res.trim().split("\n");
 }
